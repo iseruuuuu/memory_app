6dd2940d1812ddc7cb694bb270585ec992ec98c8
iseruuuuu <ryutaro.iseki23@gmail.com>
2022-07-06 10:22:19 +0900

    add: create setting screen

--- lib/screen/setting/setting_screen.dart
+++ lib/screen/setting/setting_screen.dart
@@ -1,14 +1,58 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:settings_ui/settings_ui.dart';
 
-class SettingScreen extends StatelessWidget {
+class SettingScreen extends StatefulWidget {
   const SettingScreen({Key? key}) : super(key: key);
 
   @override
+  _SettingScreenState createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends State<SettingScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-
+      appBar: AppBar(
+        backgroundColor: CupertinoColors.secondarySystemBackground,
+        elevation: 0,
+        title: const Text(
+          '設定',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          color: Colors.black,
+          iconSize: 30,
+          icon: const Icon(
+            Icons.arrow_back,
+          ),
+        ),
+      ),
+      body: SettingsList(
+        sections: [
+          SettingsSection(
+            title: const Text(''),
+            tiles: <SettingsTile>[
+              SettingsTile.navigation(
+                leading: const Icon(Icons.local_police),
+                title: const Text('ライセンス'),
+                onPressed: (context) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const LicensePage(),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
         ],
       ),
     );
