4e7ed7f6238d8e26cb90ebe1e6ce32f51ffb364e
iseruuuuu <ryutaro.iseki23@gmail.com>
2022-07-06 11:11:33 +0900

    fix: change color

--- lib/parts/title/title_button.dart
+++ lib/parts/title/title_button.dart
@@ -6,18 +6,20 @@ class TitleButton extends StatelessWidget {
     Key? key,
     required this.onPressed,
     required this.text,
+    required this.color,
   }) : super(key: key);
 
   final Function() onPressed;
   final String text;
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onPressed,
       style: ElevatedButton.styleFrom(
-        primary: Colors.white,
-        onPrimary: Colors.white,
+        primary: color,
+        onPrimary: color,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(10.0),
         ),
@@ -29,8 +31,7 @@ class TitleButton extends StatelessWidget {
           borderRadius: const BorderRadius.all(Radius.circular(25.0)),
           boxShadow: [
             BoxShadow(
-              color: Colors.white.withOpacity(0.6),
-              spreadRadius: 4,
+              color: color,
               blurRadius: 10,
               offset: const Offset(0, 3),
             ),
@@ -41,7 +42,7 @@ class TitleButton extends StatelessWidget {
             text,
             style: const TextStyle(
               fontSize: 25,
-              color: CupertinoColors.black,
+              color: CupertinoColors.white,
               fontWeight: FontWeight.bold,
             ),
           ),
